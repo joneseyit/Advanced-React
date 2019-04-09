5550edaa3e7bf4021d6b9f76cce3751a7633aaf7
Andrew Jones <joneseyit@protonmail.com>
2019-04-09 16:46:02 -0400

    Vid 20 start

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,20 @@ const Mutations = {
             } 
         }, info);
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        debugger
+        //first take a copy of updates
+        const updates = { ...args };
+        delete updates.id;
+        //run the update method
+        return ctx.db.mutation.updateItem({ 
+            data: updates,
+            where: {
+                id: args.id
+            },
+
+         }, info)
     }
 };
 
