415a650530dc3c1f56109987f7d038cf3457fb7e
Andrew Jones <joneseyit@protonmail.com>
2019-04-09 14:20:27 -0400

    trouble shooting graphql query

---
+++ sick-fits/frontend/pages/update.js
@@ -0,0 +1,15 @@
+ import UpdateItem from '../components/UpdateItem';
+
+
+const Sell = ({query}) => {
+    return (
+        <div>
+            <UpdateItem id={query.id} />
+        </div>
+    )
+
+} 
+
+
+
+export default Sell;
