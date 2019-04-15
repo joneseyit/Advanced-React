8423660eb1f65e18d80db6baaaa3d10c9dc6f29b
Andrew Jones <joneseyit@protonmail.com>
2019-04-15 17:34:54 -0400

    able to create user

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
 const Mutations = {
     async createItem(parent, args, ctx, info){
         //TODO check if they are logged in
@@ -35,6 +39,29 @@ const Mutations = {
         return ctx.db.mutation.deleteItem({ where }, info)
         //query the item first to check and see if the person owns it or if they have permission to delete it
                   
+    },
+
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        //hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the db 
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password: password,
+                permissions: { set: ['USER'] }
+            } 
+        }, info);
+        //create JWT for them
+        const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET )
+        // set the JWT as a cookie on response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 *60 * 24 * 365
+        })
+        //return user to browser 
+        return user
     }
 };
 
